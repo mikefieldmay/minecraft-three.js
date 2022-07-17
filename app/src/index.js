ed21c94dc96e380a7193e53e9ab51d081f135d2b
Mike Field-May <mikefieldmay@mikes-mbp-2.home>
2022-07-17 18:43:10 +0100

    feat: add welcome screen

--- app/src/index.js
+++ app/src/index.js
@@ -3,7 +3,13 @@ import { Game } from "./components/Game";
 import "../css/style.css";
 
 window.onload = async () => {
-  const game = new Game();
-  await game.setup();
-  game.gameLoop();
+  const helpText = document.getElementById("help");
+  const startButton = document.getElementById("startButton");
+
+  startButton.onclick = async () => {
+    helpText.classList.add("hide");
+    const game = new Game();
+    await game.setup();
+    game.gameLoop();
+  };
 };
